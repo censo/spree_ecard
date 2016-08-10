ef8d33798fb427ccfc1af134a0472b51074d206e
Adam Migodziński <adam.migodzinski@goodylabs.com>
2016-08-10 11:55:37 +0200

    fix for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
   namespace :gateway do
-    get '/ecard/:gateway_id/:order_id' => 'ecard#show'
+    get '/ecard/:gateway_id/:order_id' => 'ecard#show', as: :ecard
     get '/ecard/comeback/:gateway_id/:order_id' => 'ecard#comeback', as: :ecard_comeback
     get '/ecard/error/:gateway_id/:order_id' => 'ecard#error', as: :ecard_error
   end
